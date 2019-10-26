790205b0185a0a6a71e07f99cdc0e4a2948932a0
IvanKonchinin <51362337+IvanKonchinin@users.noreply.github.com>
2019-10-26 19:16:56 +0400

    Add files via upload

--- visitka_workzilla/script.js
+++ visitka_workzilla/script.js
@@ -23,7 +23,7 @@ box_img_1.addEventListener('click', () => {
     main_text_left_box_6.innerHTML = 'тема первого заголовка';
 });
 box_img_2.addEventListener('click', () => {
-    box_img_2.classList.toggle('box_img_mod');
+    box_img_2.classList.toggle('box_img_mod_1');
     container_text_p.innerHTML = 'Второй заголовок';
     main_text_left_box_1.innerHTML = 'тема второго заголовка';
     main_text_left_box_2.innerHTML = 'тема второго заголовка';
@@ -33,7 +33,7 @@ box_img_2.addEventListener('click', () => {
     main_text_left_box_6.innerHTML = 'тема второго заголовка';
 });
 box_img_3.addEventListener('click', () => {
-    box_img_3.classList.toggle('box_img_mod');
+    box_img_3.classList.toggle('box_img_mod_2');
     container_text_p.innerHTML = 'Третий заголовок';
     main_text_left_box_1.innerHTML = 'тема третьего заголовка';
     main_text_left_box_2.innerHTML = 'тема третьего заголовка';
@@ -43,7 +43,7 @@ box_img_3.addEventListener('click', () => {
     main_text_left_box_6.innerHTML = 'тема третьего заголовка';
 });
 box_img_4.addEventListener('click', () => {
-    box_img_4.classList.toggle('box_img_mod');
+    box_img_4.classList.toggle('box_img_mod_3');
     container_text_p.innerHTML = 'Четвертый заголовок';
     main_text_left_box_1.innerHTML = 'тема четвертого заголовка';
     main_text_left_box_2.innerHTML = 'тема четвертого заголовка';
@@ -53,7 +53,7 @@ box_img_4.addEventListener('click', () => {
     main_text_left_box_6.innerHTML = 'тема четвертого заголовка';
 });
 box_img_5.addEventListener('click', () => {
-    box_img_5.classList.toggle('box_img_mod');
+    box_img_5.classList.toggle('box_img_mod_4');
     container_text_p.innerHTML = 'Пятый заголовок';
     main_text_left_box_1.innerHTML = 'тема пятого заголовка';
     main_text_left_box_2.innerHTML = 'тема пятого заголовка';
@@ -63,7 +63,7 @@ box_img_5.addEventListener('click', () => {
     main_text_left_box_6.innerHTML = 'тема пятого заголовка';
 });
 box_img_6.addEventListener('click', () => {
-    box_img_6.classList.toggle('box_img_mod');
+    box_img_6.classList.toggle('box_img_mod_5');
     container_text_p.innerHTML = 'Шестой заголовок';
     main_text_left_box_1.innerHTML = 'тема шестого заголовка';
     main_text_left_box_2.innerHTML = 'тема шестого заголовка';
@@ -73,7 +73,7 @@ box_img_6.addEventListener('click', () => {
     main_text_left_box_6.innerHTML = 'тема шестого заголовка';
 });
 box_img_7.addEventListener('click', () => {
-    box_img_7.classList.toggle('box_img_mod_1');
+    box_img_7.classList.toggle('box_img_mod_6');
     container_text_p.innerHTML = 'О компании';
     main_text_left_box_1.innerHTML = 'О компании';
     main_text_left_box_2.innerHTML = 'О компании';
